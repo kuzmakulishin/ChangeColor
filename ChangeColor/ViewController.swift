94458fabb5b52b0192816146ce11fbdabcfc4688
Kuzma Kulishin <kuzma.zzz@gmail.com>
2020-12-04 13:39:32 +0300

    add code for UI design

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -19,8 +19,46 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 15
+        
+        redLabel.layer.masksToBounds = true
+        redLabel.layer.cornerRadius = 5
+        redLabel.textColor = .red
+        redLabel.textAlignment = .center
+        
+        greenLabel.layer.masksToBounds = true
+        greenLabel.layer.cornerRadius = 5
+        greenLabel.textAlignment = .center
+        
+        blueLabel.layer.masksToBounds = true
+        blueLabel.layer.cornerRadius = 5
+        blueLabel.textColor = .blue
+        blueLabel.textAlignment = .center
+        
+        redSlider.minimumTrackTintColor = .red
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 255
+        redSlider.value = redSlider.maximumValue / 2
+        
+        greenSlider.minimumTrackTintColor = greenLabel.textColor
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 255
+        greenSlider.value = greenSlider.maximumValue / 2
+        
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 255
+        blueSlider.value = blueSlider.maximumValue / 2
+        
+        
         
     }
     
